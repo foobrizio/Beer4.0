cb221b2688f2bb652b7ea0e879c41bb985288839
Fabrizio Gabriele <fabry.p91@gmail.com>
2021-06-26 18:14:49 +0200

    Aggiunto NTP server
    Work in progress

--- ESP32/Stocker/stocker_main.ino
+++ ESP32/Stocker/stocker_main.ino
@@ -1,10 +1,12 @@
-
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
 
 #include "WiFi.h"
 #include <PubSubClient.h>
+
+#include <NTPClient.h>
+#include <WifiUdp.h>
 
 // BME280 Temperature and Humidity sensor stuff
 Adafruit_BME280 bme; // I2C
@@ -22,6 +24,9 @@ long lastMsg = 0;
 char msg[50];
 int value = 0;
 
+//Dati per la connessione al server NTP (per l'orario)
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
 
 // Il tick è la durata base di delayTime per il nostro ESP32
 unsigned long tick=1000;
@@ -32,13 +37,28 @@ unsigned char bmeTickQty=10; //Questo è il numero di tick da ottenere per avere
 unsigned char flushTickCounter=0; //Questo è il counter per il flush dei dati via MQTT.
 unsigned char flushTickQty=60; //Ogni tot tick inviamo tutti i dati raccolti via MQTT.
 
+
+//Qui definiamo una Mappa per raccogliere e salvare i dati ricevuti dai sensori
+typedef struct{
+  float temperature;
+  float humidity;
+  float light;
+  float flame;
+  long time;
+}Map;
+
+//Creazione della Mappa e del relativo puntatore.
+Map myMap[3600];
+unsigned char mapPointer= 0;
+
 void setup() {
 
   Serial.begin(115200);
-  connectToWifi();
-  bmeStart();
-  // put your setup code here, to run once:
 
+  connectToWifi();
+  setupNTP();
+  setupMQTT();
+  bmeStart();
 }
 
 /*
@@ -55,7 +75,9 @@ void bmeStart(){
   Serial.println("BME280 Sensor found.");
 }
 
-
+/*
+ * Questo serve per connettersi alla rete WiFi
+ */
 void connectToWiFi(){
 
   Serial.print("Connecting to ");
@@ -67,7 +89,18 @@ void connectToWiFi(){
   Serial.println("Connected to the WiFi network!!");
 }
 
-
+/*
+ * Configurazione del server NTP per recuperare l'orario da internet
+ */
+void setupNTP(){
+  timeClient.begin();
+  // Set offset time in seconds to adjust for your timezone, for example:
+  // GMT +1 = 3600
+  // GMT +8 = 28800
+  // GMT -1 = -3600
+  // GMT 0 = 0
+  timeClient.setTimeOffset(3600);
+}
 
 /*
  * Configurazione del broker MQTT.
@@ -91,10 +124,7 @@ void callback(char* topic, byte* message, unsigned int length) {
   }
   Serial.println();
  
-  // Feel free to add more if statements to control more GPIOs with MQTT
- 
-  // If a message is received on the topic esp32/output, you check if the message is either "on" or "off". 
-  // Changes the output state according to the message
+  //Da qui controlliamo i messaggi che riceviamo.
   if (String(topic) == "beer4.0/stocker/1/output") {
     Serial.print("Changing output to ");
     if(messageTemp == "on"){
@@ -108,30 +138,37 @@ void callback(char* topic, byte* message, unsigned int length) {
 
 void loop() {
 
-  float temp;
-  float hum;
-
   bmeTickCounter++;
   flushTickCounter++;
+
   if(bmeTickCounter == bmeTickQty){
     //Se entriamo qui, allora facciamo una misurazione e la salviamo in locale.
-    temp = bme.readTemperature();
-    hum = bme.readHumidity();
+    myMap[mapPointer].temperature= bme.readTemperature();
+    myMap[mapPointer].humidity = bme.readHumitiy();
+    myMap[mapPointer].time = 0L;
+    mapPointer++;
     bmeTickCounter = 0;
   }
 
-  if(flushTickCounter == flushTickQty){
+  if(flushTickCounter >= flushTickQty){
     //Se entriamo qui, allora facciamo una flush dei dati locali via MQTT
-    flush();
-    flushTickCounter = 0;    
+    char result = flush();
+    if(result == 0){
+      //La flush è avvenuta con successo
+      flushTickCounter = 0;
+      mapPointer = 0;
+    }
   }
+
+  delay(tick);
 }
 
 /*
  * Il metodo flush() serve per inviare tutti i dati raccolti dall'ESP via MQTT.
  * Una volta inviati, i dati locali vengono cancellati.
+ * Il metodo ritorna l'esito dell'operazione. Se l'esito è positivo, allora i dati
+ * locali possono essere eliminati, altrimenti vengono conservati.
  */
-void flush(){
+char flush(){
 
 }
-
