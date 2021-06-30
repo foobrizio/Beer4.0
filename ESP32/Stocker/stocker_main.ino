29641f691c82593d90e72fa306fda31be364d6b2
Fabrizio Gabriele <fabry.p91@gmail.com>
2021-06-30 20:07:23 +0200

    Update -> OMA LWM2M
    Lo stocker ora invia i dati in formato OMA e riceve anche le richieste nello stesso formato.
    Rimane da implementare l'observe

--- ESP32/Stocker/stocker_main.ino
+++ ESP32/Stocker/stocker_main.ino
@@ -1,9 +1,16 @@
+#include <ArduinoJson.h>
+#include <ArduinoJson.hpp>
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <DHT.h>
 
 #include <WiFi.h>
 #include <PubSubClient.h>
+
+#include <string.h>
+
+
 
 
 // DHT11 Configuration (temperature / humitity)
@@ -16,8 +23,10 @@ DHT dht(DHTPIN,DHTTYPE);
 #define FLAME_PIN 14 //Digital
 
 //Dati per la connessione WiFi
-const char* ssid = "";
-const char* pass = "";
+const char* ssid = "Gabriele-2.4GHz";
+const char* pass = "i8Eo6zdPhvfqgzPVKo85hWP1";
+//const char* ssid = "dlink-8D53";
+//const char* pass = "Nuvola&Penny_53";
 
 //Dati per la configurazione MQTT
 const char* mqtt_server = "broker.hivemq.com";
@@ -32,28 +41,30 @@ PubSubClient client(espClient);
 
 // Qeusto boolean attiva o disattiva la scheda
 boolean active=true;
+// Questi altri boolean implementano l'observe dei vari dati
+boolean observeTemperature= true;
+boolean observeHumidity = true;
+boolean observeLight = true;
+boolean observeFlame = true;
 // Il tick è la durata base di delayTime per il nostro ESP32
 unsigned long tick=1000;
 
-uint8_t sensorTickCounter=0; //Questo è il counter per le misurazioni col bme
-uint8_t sensorTickQty=10; //Questo è il numero di tick da ottenere per avere una misurazione bme
+//Qui abbiamo 4 coppie diverse counter-soglia per i diversi tipi di dati
+uint8_t tempTickCounter=0;
+uint8_t tempTickQty=10; 
+uint8_t humTickCounter = 0;
+uint8_t humTickQty =10;
+uint8_t lightTickCounter=0;
+uint8_t lightTickQty=30; 
+uint8_t flameTickCounter=0;
+uint8_t flameTickQty=3; 
 
-uint8_t flushTickCounter=0; //Questo è il counter per il flush dei dati via MQTT.
-uint8_t flushTickQty=60; //Ogni tot tick inviamo tutti i dati raccolti via MQTT.
-
-
-//Qui definiamo una Mappa per raccogliere e salvare i dati ricevuti dai sensori
-typedef struct{
-  float temperature;
-  float humidity;
-  uint8_t light;
-  byte flame;
-  long time;
-}Map;
-
-//Creazione della Mappa e del relativo puntatore.
-Map myMap[3600];
-uint8_t mapPointer= 0;
+/* Questo è il documento JSON che creiamo con tutti i valori ricevuti dai sensori.
+ * Man mano che riceviamo valori, il documento cresce. Alla fine viene serializzato
+ * ed inviato tramite MQTT
+ */
+DynamicJsonDocument jdoc(3600);
+JsonArray array;
 
 void setup() {
 
@@ -117,24 +128,86 @@ void setupNTP(){
 void callback(char* topic, byte* message, unsigned int length) {
   Serial.println("Message arrived.");
   String messageTemp;
-   
+  
+  //Qui convertiamo il byte* message in una stringa
   for (int i = 0; i < length; i++) {
     messageTemp += (char)message[i];
   }
- 
-  //Da qui controlliamo i messaggi che riceviamo.
-  if (String(topic) == "brewIoT/stocker/1/command") {
+  //Qui convertiamo il char* topic in un char[]
+  char myTopic[50];
+  strcpy(myTopic, topic);
 
-    if(messageTemp == "refresh"){
-      flush();
+  //Qui tokenizziamo il token per navigarlo attraverso i vari levels
+  char *token = strtok(myTopic,"/"); //da qui leggeremo "cmd"
+  token = strtok(NULL,"/"); //da qui leggeremo "brewIoT"
+  token = strtok(NULL,"/"); //da qui leggeremo "st"
+  token = strtok(NULL,"/"); //da qui leggeremo "0"
+  char *objectId = strtok(NULL,"/");
+
+  if(objectId=="3301"){
+    //Illuminance
+    char *objectInstance = strtok("NULL","/");
+    if(objectInstance=="0"){
+      char *resId = strtok("NULL","/");
+      if(resId=="5700"){
+        //we want to take the value
+        char *observe = strtok("NULL","/");
+        if(observe!=NULL){
+          //we want to observe it
+        }
+        else processLight();
+      }
     }
-    else if(messageTemp == "off"){
-      active=false;
-      Serial.println("off");
+  }
+
+
+  else if(token=="3303"){
+    //Temperature
+    char *objectInstance = strtok("NULL","/");
+    if(objectInstance=="0"){
+      char *resId = strtok("NULL","/");
+      if(resId=="5700"){
+        //we want to take the value
+        char *observe = strtok("NULL","/");
+        if(observe!=NULL){
+          //we want to observe it
+        }
+        else processTemperature();
+      }
     }
-    else if(messageTemp == "on"){
-      active=true;
-      Serial.println("on");
+  }
+
+
+  else if(token=="3304"){
+    //Humidity
+    char *objectInstance = strtok("NULL","/");
+    if(objectInstance=="0"){
+      char *resId = strtok("NULL","/");
+      if(resId=="5700"){
+        //we want to take the value
+        char *observe = strtok("NULL","/");
+        if(observe!=NULL){
+          //we want to observe it
+        }
+        else processHumidity();
+      }
+    }
+  }
+
+
+  else if(token=="503"){
+    //Flame sensor
+    char *objectInstance = strtok("NULL","/");
+    if(objectInstance=="0"){
+      char *resId = strtok("NULL","/");
+      if(resId=="5700"){
+        //we want to take the value
+        char *observe = strtok("NULL","/");
+        if(observe!=NULL){
+          //we want to observe it
+        }
+        else processFlame();
+      }
     }
   }
 }
@@ -145,7 +218,7 @@ void reconnect() {
   while (!client.connected()) {
     Serial.print("Attempting MQTT connection...");
     // Attempt to connect
-    String clientId = "Esp32-Brewer";
+    String clientId = "ESP32-Stocker-0";
     if (client.connect(clientId.c_str())) {
       Serial.println("connected");
       delay(1000);
@@ -162,68 +235,110 @@ void reconnect() {
 }
 
 void subscribe(){
-  boolean result = client.subscribe("brewIoT/stocker/1/command");
-  if(result){
-    Serial.println("Subscribed!!!");
-  }
+  //With the # wildcard we subscribe to all the subtopics of each sublevel. 
+  client.subscribe("cmd/brewIoT/st/0/#");
 }
+
 
 void loop() {
 
   client.loop();
   if(active){
     Serial.println("Active");
-    sensorTickCounter++;
-    flushTickCounter++;
-    if(sensorTickCounter == sensorTickQty){
-      //Se entriamo qui, allora facciamo una misurazione e la salviamo in locale.
-      measureSensors();
+    if(observeTemperature){
+      //Qui dentro entriamo se l'observe per la temp è attivo
+      tempTickCounter++;
+      if(tempTickCounter>=tempTickQty){
+        //Time to read and send a new temperature data.
+        processTemperature();
+        tempTickCounter=0;
+      }
     }
-    if(flushTickCounter >= flushTickQty){
-      //Se entriamo qui, allora facciamo una flush dei dati locali via MQTT
-      byte result = flush();
-      if(result == 0){
-        //La flush è avvenuta con successo
-        flushTickCounter = 0;
-        mapPointer = 0;
+    if(observeHumidity){
+      //Qui entriamo se l'observe per l'umidità è attivo
+      humTickCounter++;
+      if(humTickCounter>=humTickQty){
+        //Time to read and send a humidity update.
+        processHumidity();
+        humTickCounter=0;
+
+      }
+    }
+    if(observeLight){
+      //Qui entriamo se l'observe per la luminosità è attivo
+      lightTickCounter++;
+      if(lightTickCounter>=lightTickQty){
+        //Time to read and send an illuminance update.
+        processLight();
+        lightTickCounter=0;
+      }
+    }
+    if(observeFlame){
+      //Qui entriamo se l'observe per la flame detection è attivo
+      flameTickCounter++;
+      if(flameTickCounter>=flameTickQty){
+        //Time to read and send flame updates.
+        processFlame();
+        flameTickCounter=0;
       }
     }
   }
-  
   delay(tick);
 }
 
-void measureSensors(){
-  uint8_t light = senseLight();
-  byte flame = digitalRead(FLAME_PIN);
-  myMap[mapPointer].temperature= dht.readTemperature();
-  myMap[mapPointer].humidity = dht.readHumidity();
-  myMap[mapPointer].light= light;
-  myMap[mapPointer].flame = flame;
-  myMap[mapPointer].time = 0L;
-  mapPointer++;
-  sensorTickCounter = 0;
+/*
+ * This method processes a Temperature request. It gets the value from the sensor and,
+ * the, it sends it through the respective MQTT topic. The following 4 methods work
+ * in the same way.
+ */
+void processTemperature(){
+  Serial.println("Sending temperature..");
+  float temperature= dht.readTemperature();
+  char tempString[8];
+  dtostrf(temperature, 1, 2, tempString);
+  StaticJsonDocument<256> doc;
+  doc["value"]=tempString;
+  char buffer[256];
+  size_t n = serializeJson(doc, buffer);
+  client.publish("data/brewIoT/st/0/3303/0/5700", buffer, n);
+}
+
+void processHumidity(){
+  Serial.println("Sending humidity");
+  float humidity= dht.readHumidity();
+  char humString[8];
+  dtostrf(humidity, 1, 2, humString);
+  StaticJsonDocument<256> doc;
+  doc["value"]=humString;
+  char buffer[256];
+  size_t n = serializeJson(doc, buffer);
+  client.publish("data/brewIoT/st/0/3304/0/5700", buffer, n);
 }
 
 /*
- * This method returns a percentage of the detected light, from 100 (Very Bright)
- * to 0 (Dark)
+ * This method calculates a percentage of the detected light, from 100 (Very Bright)
+ * to 0 (Dark), and forwards it through the specific MQTT topic
  */
-uint8_t senseLight(){
+void processLight(){
+  Serial.println("Sending light");
   int l = analogRead(LIGHT_PIN);
-  return map(l,4095,0,0,100);
+  uint8_t value= map(l,4095,0,0,100);
+  char lightString[8];
+  dtostrf(value, 1, 2, lightString);
+  StaticJsonDocument<256> doc;
+  doc["value"]=lightString;
+  char buffer[256];
+  size_t n = serializeJson(doc, buffer);
+  client.publish("data/brewIoT/st/0/3301/0/5700", buffer, n);
 }
-/*
- * Il metodo flush() serve per inviare tutti i dati raccolti dall'ESP via MQTT.
- * Una volta inviati, i dati locali vengono cancellati.
- * Il metodo ritorna l'esito dell'operazione. Se l'esito è positivo, allora i dati
- * locali possono essere eliminati, altrimenti vengono conservati.
- */
-byte flush(){
 
-  if (!client.connected()) {
-    reconnect();
-  }
-  client.publish("brewIoT/stocker/1/sensors","yayaya");
-  return 0;
+void processFlame(){
+  boolean flame = digitalRead(FLAME_PIN);
+  //client.publish("data/brewIoT/st/0/503/0/5700", flame);
+}
+
+int64_t get_time() {
+	struct timeval tv;
+	gettimeofday(&tv, NULL);
+	return (tv.tv_sec * 1000LL + (tv.tv_usec / 1000LL));
 }
